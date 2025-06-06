312d853b3d260432126135814456ee6e26473dd4
hepinamipara <hepinamipara7600@gmail.com>
2025-06-06 18:22:36 +0530

    change lb

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,4 +1,4 @@
- const API_BASE_URL = "http://adb22667dd9e942af890810cbe11180a-1370933755.us-east-1.elb.amazonaws.com/";
+ const API_BASE_URL = "http://a1f2b91a6c02a4c2e82ff61f7db61c16-124867296.us-east-1.elb.amazonaws.com";
 // const API_BASE_URL = "http://veera.narni.co.in";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
