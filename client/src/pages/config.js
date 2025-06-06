6e66fdbb77c48055bcf5092ba5dcb9c6f606722a
hepinamipara <hepinamipara7600@gmail.com>
2025-06-06 17:48:06 +0530

    url changed

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,4 +1,4 @@
- const API_BASE_URL = "adb22667dd9e942af890810cbe11180a-1370933755.us-east-1.elb.amazonaws.com";
+ const API_BASE_URL = "http://www.hepin.site";
 // const API_BASE_URL = "http://veera.narni.co.in";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
