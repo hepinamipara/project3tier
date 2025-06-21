1c52900dac2afc608f060739e911e62dc355a566
hepinamipara <113300990+hepinamipara@users.noreply.github.com>
2025-06-21 16:52:15 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
- // const API_BASE_URL = "http://www.hepin.site";
- const API_BASE_URL = "http://3.84.159.92:84";
+ const API_BASE_URL = "http://www.hepin.site";
+ // const API_BASE_URL = "http://3.84.159.92:84";
 // const API_BASE_URL = "http://veera.narni.co.in";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
